f552234245a61e7c4dfec9cd67f9f05a66b452f3
Daniela Sanchez barrera <danisanc@c2s1d2.42wolfsburg.de>
2022-03-14 21:45:51 +0100

    fix bugs

--- algo.c
+++ algo.c
@@ -1,25 +1,80 @@
 #include "push_swap.h"
-#define SIZE 15
+#define SIZE 5
+
+int findindexbiggest(int *stack_b, int argb)
+{
+	int i;
+	int num;
+	int	index;
+
+	i = 1;
+	index = 0;
+	num = stack_b[0];
+	while(i < argb - 1)
+	{
+		if(stack_b[i] > num)
+		{
+			num = stack_b[i];
+			index = i;
+		}
+			
+		i++;
+	}
+	return (index);
+}
+
+int findindexsmallest(int *stack_b, int argb)
+{
+	int i;
+	int num;
+	int	index;
+
+	i = 1;
+	index = 0;
+	num = stack_b[0];
+	while(i < argb - 1)
+	{
+		if(stack_b[i] < num)
+		{
+			num = stack_b[i];
+			index = i;
+		}
+			
+		i++;
+	}
+	return (index);
+}
 
 int findspot(int num, int *stack_b, int argb)
 {
 	int i;
 
 	i = 0;
-	while (i < argb -1)
+	while (i < argb - 2)
 	{
-		if ((num > stack_b[0] && num < stack_b[argb - 2]) || (num < stack_b[0] && num > stack_b[argb - 1]))
-			return (0);
+	
+		if (isbiggest(num, stack_b, argb))
+			return (findindexbiggest(stack_b, argb));
+
+
+		// if ((num > stack_b[0] && num < stack_b[argb - 2]) || (num < stack_b[0] && num > stack_b[argb - 1]))
+		// 	return (0);
 		if ((issmallest(stack_b[i], stack_b, argb) && isbiggest(stack_b[i + 1], stack_b, argb)) || (issmallest(stack_b[i + 1], stack_b, argb) && isbiggest(stack_b[i], stack_b, argb)))
 		{
 			i++;
 			continue;
 		}
-		if ((num > stack_b[i] && num < stack_b[i]) || (num < stack_b[i] && num > stack_b[i]))
+
+		//removed || (num < stack_b[i] && num > stack_b[i + 1])
+		//printf("%d vs %dxxxx %d\n", stack_b[i], stack_b[i + 1], num);
+		if ((num < stack_b[i] && num > stack_b[i + 1]))
+		{
+			//printf("%d vs %dxxxx %d\n", stack_b[i], stack_b[i + 1], num);
 			break;
+		}
 		i++;
 	}
-	return(i);
+	return(i + 1);
 }
 
 
@@ -45,6 +100,7 @@ int exploretop(int  *stack_a)
 	int	index;
 
 	i = 1;
+	index = 0;
 	num = stack_a[0];
 	while(i < SIZE - 1)
 	{
@@ -66,6 +122,7 @@ int explorebottom(int  *stack_a, int argc)
 	int	index;
 
 	i = argc - 3;
+	index = argc - 2;
 	num = stack_a[argc - 2];
 	while(i > argc - 2 - SIZE)
 	{
@@ -94,7 +151,6 @@ int	biggestnumindex(int *stack_a, int argc)
 			num = stack_a[i];
 			index = i;
 		}
-			
 		i++;
 	}
 	return (index);
@@ -110,56 +166,75 @@ void forhundred(int *stack_a, int *argc,  int *argb)
 	stack_b = ft_calloc(*argc - 1, sizeof(int));
 	while(*argc >= SIZE - 1)
 	{
+		int f = 0;
 		hold_top = exploretop(stack_a);
 		hold_bottom = explorebottom(stack_a, *argc);
-		// printf("++++++++\n");
-	 	// printarr(*argc, stack_a);
-		// printf("++++++++bottom %d\n", hold_bottom);
-		//  printf("++++++++top %d\n", hold_top);
+		//printf("++++++");
 		if (*argc - 1 - hold_bottom < hold_top)
 		{
+			f = findspot(stack_a[hold_bottom], stack_b, *argb);
 			x = *argc - 1 - hold_bottom;
 			while(x > 0)
 			{
 				rotate_r(stack_a, 'a', *argc);
 				x--;
 			}
+			if (*argb < 3)
+			{
+				push_ab(stack_a, stack_b, argc, argb);
+				write(1, "pb\n", 3);
+			}
 		}
 		else
+		{
+			f = findspot(stack_a[hold_top], stack_b, *argb);
 			while(hold_top > 0)
 			{
 				ft_rotate(stack_a, 'a', *argc);
 				hold_top--;
 			}
-		if (*argb < 4)
+			if (*argb < 3)
+			{
+				push_ab(stack_a, stack_b, argc, argb);
+				write(1, "pb\n", 3);
+			}
+		}
+
+			
+		if (*argb < 3)
 		{
 			push_ab(stack_a, stack_b, argc, argb);
 			write(1, "pb\n", 3);
 		}
 		else
 		{
-			hold_top = findspot(stack_b[0], stack_b, *argb);
-			if (hold_top == 0)
-			{	push_ab(stack_a, stack_b, argc, argb);
+			int f = 0;
+			f = findspot(stack_a[0], stack_b, *argb);
+			if (f == 0)
+			{	
+				push_ab(stack_a, stack_b, argc, argb);
 				write(1, "pb\n", 3);
 			}
 			else
 			{
-				if (hold_top < ((*argb - 2) / 2) || hold_top == ((*argb - 2) / 2) || hold_top == ((*argb - 2) / 2) + 1)
+				if (f < ((*argb - 2) / 2) || f == ((*argb - 2) / 2) || f == ((*argb - 2) / 2) + 1)
 				{
 					
-					while(hold_top >= 0)
+					while(f > 0)
 					{
+						//printf("++++ppp++++\n");
 						ft_rotate(stack_b, 'b', *argb);
-						hold_top--;
+						f--;
 					}
 				}
 				else
 				{
+					
 					int x;
-					x = *argb - 1 - hold_top;
+					x = *argb - 1 - f;
+
 					//printf("++++++++x %d\n", x);
-					while(x >= 0)
+					while(x > 0)
 					{	
 						rotate_r(stack_b, 'b', *argb);
 						x--;
@@ -174,81 +249,59 @@ void forhundred(int *stack_a, int *argc,  int *argb)
 	}
 	while(*argc != 1)
 	{
-		// if (issmallest(stack_a[0], stack_a, *argc) || isbiggest(stack_a[0], stack_a, *argc))
-		// 	ft_rotate(stack_a, 'a', *argc);
-		// if (issmallest(stack_a[0], stack_a, *argc) || isbiggest(stack_a[0], stack_a, *argc))
-		// 	ft_rotate(stack_a, 'a', *argc);
-		if (stack_a[0] > stack_a[1] && stack_b[0] < stack_b[1] && *argb > 2 && *argc > 2)
-			ft_swapab(stack_a, stack_b);
-		else if (stack_a[0] > stack_a[1] && *argc > 2)
-			ft_swap(stack_a, 'a');
-		else if (stack_b[0] < stack_b[1] && *argb > 2)
-			ft_swap(stack_b, 'b');
+		int f = 0;
+			f = findspot(stack_a[0], stack_b, *argb);
+			//printf("++++spot+++%d for %d in stack_b\n", f, stack_a[0]);
+			//printarr(*argb, stack_b);
+			//printf("++++ccc++++%d\n", hold_top);
+			if (f == 0)
+			{	
+				push_ab(stack_a, stack_b, argc, argb);
+				write(1, "pb\n", 3);
+			}
+			else
+			{
+				if (f < ((*argb - 2) / 2) || f == ((*argb - 2) / 2) || f == ((*argb - 2) / 2) + 1)
+				{
+					
+					while(f > 0)
+					{
+						//printf("++++ppp++++\n");
+						ft_rotate(stack_b, 'b', *argb);
+						f--;
+					}
+				}
+				else
+				{
+					
+					int x;
+					x = *argb - 1 - f;
 
-		// if (issmallest(stack_a[0], stack_a, *argc) || isbiggest(stack_a[0], stack_a, *argc))
-		// 	ft_rotate(stack_a, 'a', *argc);
-		
-		// if (issmallest(stack_a[0], stack_a, *argc) || isbiggest(stack_a[0], stack_a, *argc))
-		// 	ft_rotate(stack_a, 'a', *argc);
-
-		push_ab(stack_a, stack_b, argc, argb);
-		write(1, "pb\n", 3);
+					//printf("++++++++x %d\n", x);
+					while(x > 0)
+					{	
+						rotate_r(stack_b, 'b', *argb);
+						x--;
+					}
+				}
+				push_ab(stack_a, stack_b, argc, argb);
+				write(1, "pb\n", 3);
+			}
 		if (*argc == 1)
 			break;
-		
-		// if (issmallest(stack_a[0], stack_a, *argc) || isbiggest(stack_a[0], stack_a, *argc))
-		// 	ft_rotate(stack_a, 'a', *argc);
-	
-		if (stack_a[0] > stack_a[1] && stack_b[0] < stack_b[1] && *argb > 2 && *argc > 2)
-			ft_swapab(stack_a, stack_b);
-		else if (stack_a[0] > stack_a[1] && *argc > 2)
-			ft_swap(stack_a, 'a');
-		else if (stack_b[0] < stack_b[1] && *argb > 2)
-			ft_swap(stack_b, 'b');
-		if (*argc == 1)
-			break;
-		// if (issmallest(stack_b[0], stack_b, *argb) && *argb > 2)
-		// 	ft_rotate(stack_b, 'b', *argc);
 	}
-	// while(*argc == 4)
-	// {
-	// 	if (!(isbiggest(stack_a[0], stack_a, *argc)) && !(issmallest(stack_a[0], stack_a, *argc)))
-	// 	{
-	// 		push_ab(stack_a, stack_b, argc, argb);
-	// 		write(1, "pb\n", 3);	
-	// 	}
-	// 	else
-	// 		if (!(isbiggest(stack_a[1], stack_a, *argc)) && !(issmallest(stack_a[1], stack_a, *argc)))
-	// 			if (stack_a[0] > stack_a[1] && stack_b[0] < stack_b[1] && *argb > 2 && *argc > 2)
-	// 				ft_swapab(stack_a, stack_b);
-	// 			else
-	// 				ft_swap(stack_a, 'a');
-	// 		else
-	// 			rotate_r(stack_a, 'a', *argc);
 
-	// }
 	while(*argb > 1)
-	{\
-		
-		// if (issmallest(stack_a[0], stack_a, *argc))
-		// 	ft_swap(stack_a, 'a');
-		// if (stack_b[0] < stack_b[1])
-		// 	ft_swap(stack_b, 'b');
+	{
 		if (isbiggest(stack_b[0], stack_b, *argb))
 		{
 			push_ab(stack_b, stack_a, argb, argc);
 			write(1, "pa\n", 3);
-
-			// printf("++++++++\n");
-			// printarr(*argb, stack_b);
-	
 		}
 		else
 			{
 				int index;
 				index = biggestnumindex(stack_b, *argb);
-				//printf("++++++++index %d\n", index);
-				//printf("++++++++x %d\n", (*argb - 2) / 2);
 				if (index < ((*argb - 2) / 2) || index == ((*argb - 2) / 2) || index == ((*argb - 2) / 2) + 1)
 				{
 					while(index > 0)
@@ -262,10 +315,8 @@ void forhundred(int *stack_a, int *argc,  int *argb)
 				{
 					int x;
 					x = *argb - 1 - index;
-					//printf("++++++++x %d\n", x);
 					while(x > 0)
 					{
-						
 						rotate_r(stack_b, 'b', *argb);
 						x--;
 					}
@@ -273,9 +324,6 @@ void forhundred(int *stack_a, int *argc,  int *argb)
 
 			}
 	}
-	//rotate_r(stack_a, 'a', *argc);
-	// printf("++++++++\n");
-	// printarr(*argc, stack_a);
 }
 
 int main(int argc, char **argv)
