435b531fac2f51d4d26e6511de369ebae855c001
Daniela Sanchez barrera <danisanc@c2s1d2.42wolfsburg.de>
2022-03-24 21:52:11 +0100

    fix header file

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: danisanc <danisanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 15:19:42 by danisanc          #+#    #+#             */
-/*   Updated: 2022/03/24 21:43:15 by danisanc         ###   ########.fr       */
+/*   Updated: 2022/03/24 21:51:52 by danisanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,5 +84,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *));
-long	ft_atol(const char *str);
+//////////////////////////////////
+long int	ft_atol(const char *str);
 #endif
